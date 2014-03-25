d2691bf7e5e91ba4f71a3adbe196de183fd1cedb
sebabelmar <si.belmar@gmail.com>
2014-03-24 17:46:40 -0700

    2nd challenge update 4

--- 2_calculate_letter_grade/my_solution.rb
+++ 2_calculate_letter_grade/my_solution.rb
@@ -8,22 +8,27 @@
 
 # 1. Pseudocode
 
-# The input is an ARRAY of numbers (0 to 100)
-# The output is a letter grade that represents the average 
+# The input is an ARRAY of numbers (0 to 100).
+# The output is a letter grade that represents the average. 
 # of the numbers in the ARRAY.
 
-# DEFINE a method and name it get_grade that will pass an array as a parameter
-# IN THE BODY of the method DEFINE a variable, name it result and give it a value = to 0
-# CALL the each method to the array and create a small block of code inside {} where 
+# DEFINE a method and name it get_grade that will pass an array as a parameter.
+
+# IN THE BODY of the method DEFINE a variable, name it result and give it a value = to 0.
+
+# CALL the each method to the array and create a small block of code inside {} where
 # you define each element of the array as x using || right after that make result to increase +=
 # the value of x on each iteration.
+
 # DEFINE average by dividing result by array length.
-# USE the condition statement CASE for the variable average to return a letter grade
+
+# USE the condition statement CASE for the variable average to return a letter grade.
 # when average is between 90 and 100 return A
 # when average is between 80 and 89 return B
 # when average is between 70 and 79 return C
 # when average is between 60 and 69 return D
 # Otherwise return F
+
 # END make sure that you end the method
 
 
