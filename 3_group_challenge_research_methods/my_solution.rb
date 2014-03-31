50f311ef1bd5122aeda44416bb93c4f0b6316842
sebabelmar <si.belmar@gmail.com>
2014-03-30 22:32:02 -0700

    Group Update

--- 3_group_challenge_research_methods/my_solution.rb
+++ 3_group_challenge_research_methods/my_solution.rb
@@ -1,17 +1,15 @@
-# # U1.W3: Research Methods
+# U1.W3: Research Methods
 
 i_want_pets = ["I", "want", 3, "pets", "but", "I", "only", "have", 2]
-my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, 
+my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4,
             "Annabelle" => 0, "Ditto" => 3}
 
 # Person 1's solution
 def my_array_finding_method(ary, thing_to_find)
-  # Your code here!
   ary.select {|word| word.to_s.include?(thing_to_find)}
 end
 
 def my_hash_finding_method(hash, age)
-  # Your code here!
   hash.select {|k,v| v == age}.keys
 end
 
@@ -20,8 +18,6 @@ end
 # I used String#include? method which takes a parameter and returns true if there is a match and false otherwise 
 # I used Hash#select which is same as Array#select but takes both key value pairs as block parameters
 # I used Hash#keys method which returns list of keys of corresponding hash in an Array
-
-# Person 2
 
 def my_hash_modification_method(source, things_to_modify)
   source.each {|k, v| source[k] = v + things_to_modify}
@@ -78,19 +74,7 @@ def my_hash_deletion_method(source, thing_to_delete)
 	source.delete_if {|k, v| k.to_s == thing_to_delete}
 end
 
-def my_deletion_method(source, thing_to_delete)	
-	
-	if source.class.to_s == "Array"
-	my_array_deletion_method(source, thing_to_delete)
-	end
 
-	if source.class.to_s == "Hash"
-	my_hash_deletion_method(source, thing_to_delete)
-	end
-
-	return source
-
-end
 # Identify and describe the ruby method you implemented. 
 # Array#delete_if and Hash#delete_if which takes a block and deletes elements from an array
 # or Hash related to any condition written in the blok.
@@ -100,7 +84,7 @@ end
 
 
 ################## DRIVER CODE ###################################
-# HINT: Use `puts` statements to see if you are altering the original structure with these methods. 
+# HINT: Use `puts` statements to see if you are altering the original structure with these methods.
 # Each of these should return `true` if they are implemented properly.
 
 p my_array_finding_method(i_want_pets, "t") == ["want","pets","but"]
@@ -108,7 +92,20 @@ p my_hash_finding_method(my_family_pets_ages, 3) == ["Hoobie", "Ditto"]
 p my_array_modification_method(i_want_pets, 1) == ["I", "want", 4, "pets", "but", "I", "only", "have", 3 ]
 p my_hash_modification_method(my_family_pets_ages, 2) == {"Evi" => 8, "Hoobie" => 5, "George" => 14, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
 p my_array_sorting_method(i_want_pets) == ["3", "4", "I", "but", "have", "only", "pets", "want"]
-p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]] 
+p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]]
 # This may be false depending on how your method deals with ordering the animals with the same ages.
-p my_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "I", "only", 3 ]
-p my_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
+p my_array_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "I", "only", 3 ]
+p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
+
+# Reflect!
+# This was a great challenge. On top of having to keep track of the changes that this 
+# challenge got during the week, we had to work as a team.
+# Even if we tried to avoid waiting on each other we end up having to hang out any 
+# ways. This made me realize that if we are depending on someone’s answered its 
+# better to strategize the development a little better. I think this time we didn’t read 
+# what others had to do until we got in troubles and that might be the lesson here.
+# How complex this challenge was I think it related more to what each one of us was 
+# able to learn about Hashes and Array, plus any other thing in the dynamics of the 
+# activity. For me was fun and I got to see some things about teamwork that I might 
+# have missed otherwise. Our team developed interesting solution and we collaborate 
+# a lot, both thing that make me feel proud. 
